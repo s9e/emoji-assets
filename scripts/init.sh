6f4d50106d8799743c4c1f78fb3ceff75ef76071
JoshyPHP <s9e.dev@gmail.com>
2017-11-19 01:38:05 +0100

    Updated init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,15 +1,6 @@
 #/bin/bash
 
-EMOJI_DIR="third_party/googlei18n/noto-emoji"
-
 cd "$(dirname $(dirname $0))"
 
-if [ -d "$EMOJI_DIR" ];
-then
-	GIT_DIR="$EMOJI_DIR/.git" git pull
-else
-	git clone --depth=1 https://github.com/googlei18n/noto-emoji.git "$EMOJI_DIR"
-fi
-
-npm install --prefix third_party "git+https://git@github.com/JoshyPHP/svgo.git#patched"
-#npm install --prefix third_party "git+https://git@github.com/JoshyPHP/svgo.git#patched" twemoji
+git submodule update --init --recursive
+npm install --prefix third_party svgo
